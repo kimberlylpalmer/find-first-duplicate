ef9dd7c4dcb08b34ecbaf8774b7fde55bbdde1ef
Kimberly Palmer <kimberlylpalmer@gmail.com>
2024-04-01 17:02:47 -0700

    Day 2

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,9 +1,22 @@
 function findFirstDuplicate(arr) {
   // type your code here
+  const individual = new Set();
+
+  for (const value of arr) {
+    if (individual.has(value)) {
+      return value;
+    }
+    individual.add(value);
+  }
+  return -1;
+
 }
 
 if (require.main === module) {
   // add your own tests in here
+  console.log("Expecting: 4")
+  console.log(findFirstDuplicate([4, 1, 2, 3, 4, 1, 2, 3, 4]))
+
   console.log("Expecting: 3");
   console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
 
@@ -16,4 +29,10 @@ if (require.main === module) {
 module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
+// function findFirstDuplicate(arr) {
+//   set const variable individual to equal a new empty set to store unique values
+//  loop through the array to check each value
+//  if the value is listed in individual then it will return the value 
+//   for each value in the arr provided it should return the value
+// }
 // And a written explanation of your solution
